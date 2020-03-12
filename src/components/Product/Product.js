f22e43e629ed4617f11fed4c0b9827c726ba4925
avishek-ayon <avishek.ayon96@gmail.com>
2020-03-13 00:31:30 +0600

    half1

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import './Product.css';
 const Product = (props) => {
     console.log(props.product.category);
+    const {img,name,seller,price,stock}=props.product;
     return (
-        <div>
+        <div className="product">
+            <div>
+                <img src={img} alt=""/>
+            </div>
             {/* <h1>This is Product</h1> */}
-            <h3>{props.product.name}</h3>
+            <div>
+                <h3 className="product-name">{name}</h3>
+                <br/>
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <p><small>Only {stock} lest in stock - Order soon</small></p>
+            </div>
+            
         </div>
     );
 };
